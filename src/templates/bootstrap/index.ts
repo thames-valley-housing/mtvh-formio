21a559be9c3c2c24df858c4481a53fd8d9f32484
Matthew Cohen <matthew_cohen@tvha.co.uk>
2021-09-27 20:36:53 +0100

    Removed checkmatrix

--- src/templates/bootstrap/index.ts
+++ src/templates/bootstrap/index.ts
@@ -3,10 +3,9 @@ import field from './field';
 import wizard from './wizard';
 import wizardNav from './wizardNav';
 import wizardHeader from './wizardHeader';
-//import checkmatrix from './checkmatrix';
 import mtvhAddress from './mtvhAddress';
 
 
 export default {
-  label,field,wizardNav,wizard,wizardHeader,mtvhAddress//,checkmatrix
+  label,field,wizardNav,wizard,wizardHeader,mtvhAddress
 };
